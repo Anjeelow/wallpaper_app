78a3e95774087105b88416f4b40283231b72ae91
KKudas <22100890@usc.edu.ph>
2024-05-11 10:59:37 +0800

    test123

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -63,7 +63,6 @@ export default function HomeScreen({navigation}) {
   };
 
   const loadNextPage = () => {
-    console.log(`End reached, Page: ${currentPage}`);
     setCurrentPage(currentPage + 1);
   };
 
