d2aac00e9ed2d69e8a0e70bb1480421160b7b64e
KKudas <22100890@usc.edu.ph>
2024-05-11 09:05:48 +0800

    Applied prettier to other screen and fixed loading wallpaper. Can now infinity loading

---
+++ screens/ViewWallpaperScreen.js
@@ -0,0 +1,12 @@
+import {
+  Pressable,
+  View,
+  Text,
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import React, {useEffect, useState} from 'react';
+import {Style} from '../styles/Global';
